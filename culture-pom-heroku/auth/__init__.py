39e5291740011c7eb15633468fe80f92c090799f
3force-coder <3force@culturepom.com>
2025-11-26 13:44:40 +0100

    Update __init__.py

--- culture-pom-heroku/auth/__init__.py
+++ culture-pom-heroku/auth/__init__.py
@@ -1,3 +1,3 @@
 from .authenticator import show_login, is_authenticated
-from .roles import has_permission, is_admin
-__all__ = ['show_login', 'is_authenticated', 'has_permission', 'is_admin']
+from .roles import has_permission, is_admin, is_compteur
+__all__ = ['show_login', 'is_authenticated', 'has_permission', 'is_admin', 'is_compteur']
