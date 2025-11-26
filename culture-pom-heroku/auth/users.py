55f55d9e1f802a75723a3aa609d1b5f961700ea7
3force-coder <3force@culturepom.com>
2025-11-26 12:50:03 +0100

    Update users.py

--- culture-pom-heroku/auth/users.py
+++ culture-pom-heroku/auth/users.py
@@ -14,5 +14,11 @@ USERS = {
         'email': 'judumas10@gmail.com',
         'password_hash': '$2b$12$7nQ4v63pW2TyMxiriADBw.HhxWBySs6IhzJg7XaUh2bNdpoc2a4kO',
         'role': 'USER'
+    },
+    'compteur_inventaire': {
+        'name': 'Compteur Inventaire',
+        'email': 'compteur@culturepom.com',
+        'password_hash': '$2b$12$KQX7jMnP9vR2wH5nYzEuXeF4L7GkM3pN8qQ5rS6tT9uU2vW0xX1yY2',
+        'role': 'COMPTEUR'
     }
 }
