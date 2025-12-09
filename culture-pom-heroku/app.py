717d1d05ff34f1ab13642d23a0201fd6a10bae0f
3force-coder <3force@culturepom.com>
2025-12-09 11:46:50 +0100

    Update app.py

--- culture-pom-heroku/app.py
+++ culture-pom-heroku/app.py
@@ -107,6 +107,7 @@ pages = {
         st.Page("pages/23_CRM_Visites.py", title="Visites", icon="📅"),
         st.Page("pages/24_CRM_Animations.py", title="Animations", icon="🎉"),
         st.Page("pages/25_CRM_Statistics.py", title="Statistiques", icon="📈"),
+        st.Page("26_CRM_Magasins_API_ADRESSE.py", title="Clients_test", icon="🏪"),
     ],
     "💰 Finance": [
         st.Page("pages/09_Valorisation_Lots.py", title="Valorisation Lots", icon="💰"),
