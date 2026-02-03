077bcb1178f2dbaceb9c1f65d0331543cc2deae1
3force-coder <3force@culturepom.com>
2026-02-03 06:01:44 +0100

    Update __init__.py

--- culture-pom-heroku/streamlit_fullcalendar/__init__.py
+++ culture-pom-heroku/streamlit_fullcalendar/__init__.py
@@ -1,1 +1,28 @@
+import streamlit.components.v1 as components
+import os
 
+_component_func = components.declare_component(
+    "fullcalendar",
+    path=os.path.join(os.path.dirname(__file__), "frontend")
+)
+
+def fullcalendar(
+    events=None,
+    external_events=None,
+    initial_date=None,
+    locale='fr',
+    editable=True,
+    droppable=True,
+    key=None
+):
+    component_value = _component_func(
+        events=events or [],
+        external_events=external_events or [],
+        initial_date=initial_date,
+        locale=locale,
+        editable=editable,
+        droppable=droppable,
+        key=key,
+        default=None
+    )
+    return component_value
